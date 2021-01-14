f7d196881d470347172a47e768d85cffc2728627
jerryliu208 <58102899+jerryliu208@users.noreply.github.com>
2021-01-15 06:45:01 +0800

    vincent

--- models/seeder/seeder.js
+++ models/seeder/seeder.js
@@ -8,7 +8,7 @@ const userJson = require('./user.json')
 // 添加會員、會議室假資料到資料庫
 
 // mongoose.set('debug', true)
-mongoose.connect('mongodb://vincent-sa-db:oSggFdJu1o6dBXNfXdqgQW9YzQgO9g3HNCqYbhDrooKYpghEkdg78tWzc1deX6807eRu6nU7wdq7jBsfXTxc1w%3D%3D@vincent-sa-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@vincent-sa-db@', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://sasawalsincloud:qj8Ab606SMDgcZTYSHnEY01KSJzMrSVD4CXEgSQJD7NEDYkCW8VzzBZ11GbtRu3VRo1wjX2ctronsheBdAUJLQ==@sasawalsincloud.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@sasawalsincloud@', { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.connection.once('open', () => { // 連線成功
     console.log('資料庫連線成功！ mongodb://localhost/walsin-cloud')
