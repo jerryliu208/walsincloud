76e14bd2b554e62a5eb07f0655ecbbf142f50295
vincent liu <jerryliu208@gmail.com>
2021-01-16 01:36:50 +0800

    vincent

--- config/multer.js
+++ config/multer.js
@@ -5,7 +5,7 @@ const multer = require(`multer`)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) { // 定義上傳的路徑
         //cb(null, './upload') // 上傳檔案至upload資料夾
-        cb(null,path.join(__dirname,'../upload'))
+        cb(null,path.join(__dirname,'./file/upload'))
     },
     filename: function (req, file, cb) { // 定義檔名
         //cb(null, `${Date.now()}-${file.originalname}`) // 將檔名設為：現在的時間(timestamp格式)-原始檔名
